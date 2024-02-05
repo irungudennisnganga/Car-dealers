a7926404789cb82d6f7ee87bbd3be75d6ccf48f7
+ <+>
2024-02-05 21:33:31 +0300

    added the basics of the project

---
+++ client/src/components/App.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import SignUp from "./SignUp";
+import Login from "./Login";
+import NavBar from "./NavBar";
+import Home from "./Home";
+
+function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // auto-login
+    fetch("/check_session").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  return (
+    <>
+      <NavBar user={user} setUser={setUser} />
+      {/* <main>
+        {user ? (
+          <Routes>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Routes>
+        ) : (
+          <Routes>
+            <Route path="/signup">
+              <SignUp setUser={setUser} />
+            </Route>
+            <Route path="/login">
+              <Login setUser={setUser} />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Routes>
+        )}
+      </main> */}
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/Home.js
@@ -0,0 +1,13 @@
+
+function Home({ user }) {
+  if (user) {
+    return <h1>hi</h1>
+    
+  } else {
+    return <h1>Login or Sign Up</h1>;
+  }
+
+  // return <dennis />
+}
+
+export default Home;
