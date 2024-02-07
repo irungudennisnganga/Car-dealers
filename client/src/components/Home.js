2f86f1f0d318de7b8a4f557562b7a7b84bb39453
+ <+>
2024-02-08 00:24:22 +0300

    added a comment session

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -8,7 +8,7 @@ function Home({ user }) {
   } else {
     return (
       <React.Fragment>
-      <h2 className="heading">LOG IN OR SIGN UP TO CONTINUE WITH US !!</h2>
+      <h2 className="heading-logout">LOG IN OR SIGN UP TO CONTINUE WITH US !!</h2>
       <Link className="links" to="/signup">Signup</Link> <br />
       <Link className="links" to="/login">Login</Link> <br />
       <img className="image-login" alt="security" src="https://media.istockphoto.com/id/164090163/photo/security-button-key.jpg?s=2048x2048&w=is&k=20&c=Ysf0idsuJZOO2u2XFL8iAjWU1N4FQJ_rg6OO6ZCzYbs=" />
