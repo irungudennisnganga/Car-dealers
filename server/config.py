3e532d029f5cf52c1723784d50ad54cbdcb532fb
+ <+>
2024-02-09 18:51:01 +0300

    added cors to the database

--- server/config.py
+++ server/config.py
@@ -3,13 +3,13 @@ from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import CORS
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
-
+CORS(app)
 db = SQLAlchemy()
 migrate = Migrate(app, db)
 db.init_app(app)
